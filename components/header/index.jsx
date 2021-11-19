5628150c95928c17d13b04158a4a8c43b2349955
JSC <gbs04087@gmail.com>
2021-11-19 14:28:43 +0900

    개발용 설정 세팅

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <div className="flex mt-3 w-full">
           <ul className="flex ml-16 ">
             <li className="text-5xl">OmegaComsumer</li>
-            <li className="text-2xl m-3 px-16">공지사항asdf</li>
+            <li className="text-2xl m-3 px-16">공지사항</li>
             <li className="text-2xl m-3">게시판</li>
           </ul>
           <ul>
