99177c6fb0e5bfbd10b2317a4ba3a915622b8379
TypingOK <wjdwls717@gmail.com>
2021-12-01 21:21:16 +0900

    게시글 목록 가져오기

--- components/homePageContainer/contents/homePageContents.jsx
+++ components/homePageContainer/contents/homePageContents.jsx
@@ -2,14 +2,35 @@ import React, { useEffect, useState } from "react";
 import HomepagePost from "./homepagePost";
 import axios from "axios";
 const HomePageContent = () => {
+  const [noticeBoard, setNoticeBoard] = useState({});
+  const [etcBoard, setEtcBoard] = useState({});
   useEffect(() => {
     (async () => {
       try {
-        console.log("asdf2");
         const res = await axios.get("/api/post/getPosts");
         if (res.data.success) {
           const posts = res.data.post;
-          console.log(posts);
+          const serverToClientNoticeBoard = [];
+          const serverToClientEtcBoard = [];
+          posts.forEach((element) => {
+            if (element.category === "noticeBoard") {
+              serverToClientNoticeBoard.push({
+                title: element.title,
+                uuid: element.uuid,
+              });
+            } else {
+              serverToClientEtcBoard.push({
+                title: element.title,
+                uuid: element.uuid,
+              });
+            }
+          });
+          if (serverToClientNoticeBoard.length !== 0) {
+            setNoticeBoard(serverToClientNoticeBoard);
+          }
+          if (serverToClientEtcBoard.length !== 0) {
+            setEtcBoard(serverToClientEtcBoard);
+          }
         } else {
           console.log("서버가 이상이 생겨 포스트를 못가져옴");
         }
@@ -22,8 +43,8 @@ const HomePageContent = () => {
   return (
     <div className="flex flex-col w-full">
       <div className="flex flex-col lg:flex-row justify-center ">
-        <HomepagePost />
-        <HomepagePost />
+        <HomepagePost mainPageContentTitle={noticeBoard} />
+        <HomepagePost mainPageContentTitle={etcBoard} />
       </div>
       <div>안녕안녕</div>
     </div>
