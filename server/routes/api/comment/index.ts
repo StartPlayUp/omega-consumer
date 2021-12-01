d96cba15c9ba1b62a2637c29ecc71bc5bc716851
JSC <gbs04087@gmail.com>
2021-12-01 16:46:51 +0900

    test

--- server/routes/api/comment/index.ts
+++ server/routes/api/comment/index.ts
@@ -10,5 +10,4 @@ router.post('/sendMemberComment', sendMemberComment);
 router.post('/sendNonMemberComment', sendNonMemberComment);
 router.get('/getComments', getComments);
 
-
 export default router
